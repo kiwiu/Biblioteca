6fda2eb1bdc57478d2029051ac43c22f6515e15c
kiwiu <ramosjeferson281@gmail.com>
2023-10-05 17:08:15 -0600

    creacion de splash

--- app/src/main/java/com/example/biblioteca/MainActivity.java
+++ app/src/main/java/com/example/biblioteca/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.biblioteca.Views;
+package com.example.biblioteca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.biblioteca.R;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
     }
 }
